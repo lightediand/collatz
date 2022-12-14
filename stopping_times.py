361ad2c747e81dd1e8991cd08d2a351f81e5348f
Light Ediand <lightediand@mail.com>
2022-12-14 21:58:27 +0000

    created stopping time function

--- stopping_times.py
+++ stopping_times.py
@@ -1,19 +1,15 @@
-pos_int = input("Please specify a positive integer to generate the sequence: ")
+def stopping_time(z: int):
+    s=[]
+    s.clear()
+    s.append(z)
+    while z>1:
+        if z%2==0:
+            n=z//2
+            s.append(n)
+            z=n
+        else:
+            n=3*z+1
+            s.append(n)
+            z = n
+    print(len(s)-1)
 
-z = int(pos_int)
-
-sequence = []
-sequence.append(z)
-
-while z > 1:
-    if z % 2 == 0:
-        n = z / 2
-        sequence.append(int(n))
-        z = n
-    else:
-        n = 3*z + 1
-        sequence.append(int(n))
-        z = n
-
-print("The sequence is ", sequence)
-print("The stopping time is ", len(sequence))
