6184d6d0158eb1ed5fca010857c7e20533c0bc02
Light Ediand <lightediand@mail.com>
2022-12-14 22:34:20 +0000

    update sequence generator

--- sequence_generator.py
+++ sequence_generator.py
@@ -1,15 +1,13 @@
-pos_int = input("Please specify a positive integer to generate the sequence: ")
-
-z = int(pos_int)
-
-print(z)
-
-while z > 1:
-    if z % 2 == 0:
-        n = z / 2
-        print(int(n))
-        z = n
-    else:
-        n = 3*z + 1
-        print(int(n))
-        z = n
+def sequence_generator(z: int):
+    sequence = []
+    sequence.append(z)
+    while z > 1:
+        if z % 2 == 0:
+            n = z // 2
+            sequence.append(n)
+            z = n
+        else:
+            n = 3*z + 1
+            sequence.append(n)
+            z = n
+    print(sequence)
