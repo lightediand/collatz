5478180e9c3c45c78566cc829ff8c030302685b7
Light Ediand <lightediand@mail.com>
2023-04-20 19:15:21 +0100

    while loop

--- collatz.py
+++ collatz.py
@@ -1,10 +1,14 @@
 from sequence_generator import sequence_generator as sg
 from stopping_times import stopping_time as st
 
-test_value = input("Please specify a number to test: ") 
-
-t = int(test_value)
-
-print("For the test value", t, "the stopping time is", st(t), "and the sequence is:\n")
-print(sg(t))
-print("\nUnfortunately, this number is not a counterexample to the Collatz conjecture. Try again!")
+print("Welcome to the Collatz Conjecture Counterexample Finder!")
+while True:
+    test_value = input("Please specify a number to test, or type exit to quit: ")
+    if test_value == str("exit"):
+       print("Have a nice day!")
+       break
+    else:
+       t = int(test_value)
+       print("\nFor the test value", t, "the stopping time is", st(t), "and the sequence is:")
+       print(sg(t))
+       print("\nUnfortunately, this number is not a counterexample to the Collatz Conjecture. Try again!\n")
